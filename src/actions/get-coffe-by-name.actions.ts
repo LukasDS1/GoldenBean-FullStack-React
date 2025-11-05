9dc02e66b75abac001738cce4b2b9ed14d64dc74
EndorphinEuphoria <be.moretti@duocuc.cl>
2025-11-05 16:21:01 -0300

    Added normalized name to api database and changed getcoffebyname action
    Changed get-coffe-by-name.actions to search by normalizedname, and added a new helper function to normalize the query made by the user.

--- src/actions/get-coffe-by-name.actions.ts
+++ src/actions/get-coffe-by-name.actions.ts
@@ -1,11 +1,12 @@
+import { normalized } from '../helpers/normalize.helpers';
 import type { coffeeProps } from '../interfaces/coffe.interfaces'; 
-import { capitalizeFirst } from '../helpers/capitalize-first.helpers'
+
 
 export const getCoffeeByName = async (query: string): Promise<coffeeProps[]> => {
-  const capitalized = capitalizeFirst(query);
-  const encodedName = encodeURIComponent(capitalized);
-  
-  const response = await fetch(`http://localhost:3001/coffe?nombre_like=${encodedName}`);
+  const normalize = normalized(query)
+  const encodedName = encodeURIComponent(normalize);
+  console.log(normalize)
+  const response = await fetch(`http://localhost:3001/coffe?nombre_normalizado_like=${encodedName}`);
   if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
   
   const data:coffeeProps[] = await response.json();
---
+++ src/helpers/normalize.helpers.ts
@@ -0,0 +1,7 @@
+export function normalized (s:string):string { 
+    return s
+    .normalize('NFD') // Separa carácter de tilde
+    .replace(/[\u0300-\u036f]/g, '') // quita 
+    .trim(); // quita espacios delante y detrás del texto, no entremedio
+    
+  }
