f69d8edab6ca0288b7ea8547c00a1027c40bdbe6
LukasDS1 <ldonoso472@gmail.com>
2025-11-05 19:54:50 -0300

    fix img}

--- src/pages/coffesComponents/imgComponent.tsx
+++ src/pages/coffesComponents/imgComponent.tsx
@@ -5,10 +5,17 @@ interface Props {
 
 export const ImgComponent = ({ imagen, nombre }: Props) => {
   return (
-    <img 
-      src={imagen} 
-      className="card-img-top" 
-      alt={nombre} 
-    />
+    <img
+  src={imagen}
+  className="card-img-top"
+  alt={nombre}
+  style={{
+    height: "250px",
+    width: "100%",
+    objectFit: "cover",
+    objectPosition: "center",
+    borderBottom: "1px solid rgba(255,255,255,0.1)"
+  }}
+/>
   )
 }
