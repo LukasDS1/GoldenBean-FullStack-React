36d1fd3f69436bd3843459312001a17e3b9f9fb1
Benjamin Moretti <be.moretti@duocuc.cl>
2025-11-05 22:03:57 -0300

    Remove debug log from getCoffeeByName function
    saqué esto y ya me void 👀

--- src/actions/get-coffe-by-name.actions.ts
+++ src/actions/get-coffe-by-name.actions.ts
@@ -5,7 +5,6 @@ import type { coffeeProps } from '../interfaces/coffe.interfaces';
 export const getCoffeeByName = async (query: string): Promise<coffeeProps[]> => {
   const normalize = normalized(query)
   const encodedName = encodeURIComponent(normalize);
-  console.log(normalize)
   const response = await fetch(`http://localhost:3001/coffe?nombre_normalizado_like=${encodedName}`);
   if (!response.ok) throw new Error(`Error HTTP: ${response.status}`);
   
